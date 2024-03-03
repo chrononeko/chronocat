3a5aa74378d3701be2a05d251e396497fb37bb3b
Il Harper <hi@ilharper.com>
2024-03-03 23:49:16 +0800

    chore: add jest config

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type { JestConfigWithTsJest } from 'ts-jest'
+
+const config: JestConfigWithTsJest = {
+  preset: 'ts-jest/presets/default-esm',
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.m?[tj]sx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+}
+
+// eslint-disable-next-line import/no-default-export
+export default config
