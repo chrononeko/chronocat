3611c745ecf7962f1c81c5b427f336906f01d21d
Il Harper <hi@ilharper.com>
2024-03-06 23:39:29 +0800

    refactor: extract ipcman handler

---
+++ packages/engine-chronocat-api/src/handler.ts
@@ -0,0 +1,217 @@
+import type {
+  MsgsIncludeSelf,
+  OnAddSendMsg,
+  OnBuddyListChange,
+  OnGroupListUpdate,
+  OnMemberInfoChange,
+  OnMemberListChange,
+  OnMsgInfoListUpdate,
+  OnOpenParamChange,
+  OnProfileChanged,
+  OnRecentContactListChangedVer2,
+  OnRecvMsg,
+  OnRichMediaDownloadComplete,
+  RedIpcDataEvent,
+  RedIpcDataRequest,
+} from '@chronocat/red'
+import { ChatType } from '@chronocat/red'
+import type { ChronocatContext } from '@chronocat/shell'
+import type { IpcManData } from 'ipcman'
+import {
+  chronoEventEmitter,
+  friendMap,
+  groupMap,
+  requestCallbackMap,
+  requestMethodMap,
+  richMediaDownloadMap,
+  sendCallbackMap,
+  sendQueue,
+} from './globalVars'
+
+export const buildHandler = (ctx: ChronocatContext) => (data: IpcManData) => {
+  switch (data.type) {
+    case 'event': {
+      if (!data.args[1] || !Array.isArray(data.args[1])) return
+      const d = data.args[1] as [RedIpcDataEvent]
+      if (!d.length) return
+      const e = d[0]
+      if (!e || !('cmdName' in e)) return
+      void dispatcher(ctx, e.cmdName, e.payload)
+      return
+    }
+
+    case 'wrapped-request': {
+      requestMethodMap[data.id] = (data.args[1] as RedIpcDataRequest)[0]
+      return
+    }
+
+    case 'wrapped-response': {
+      const res = requestCallbackMap[data.id]
+      if (res) res(data.args[1] /* RedIpcDataResponse */)
+
+      const method = requestMethodMap[data.id]
+      if (method) {
+        delete requestMethodMap[data.id]
+        void dispatcher(ctx, method, data.args[1] /* RedIpcDataResponse */)
+      }
+
+      return
+    }
+  }
+}
+
+const dispatcher = async (
+  ctx: ChronocatContext,
+  method: string,
+  payload: unknown,
+) => {
+  switch (method) {
+    case 'nodeIKernelMsgListener/onRecvMsg': {
+      const { msgList } = payload as OnRecvMsg
+
+      for (const msg of msgList) {
+        ctx.chronocat.uix.add(msg.senderUid, msg.senderUin)
+        if (msg.chatType === ChatType.Private)
+          ctx.chronocat.uix.add(msg.peerUid, msg.peerUin)
+      }
+
+      return
+    }
+
+    case 'nodeIKernelMsgListener/onRichMediaDownloadComplete': {
+      const { notifyInfo } = payload as OnRichMediaDownloadComplete
+
+      const downloadId = `${notifyInfo.msgId}::${notifyInfo.msgElementId}`
+
+      if (notifyInfo.fileDownType === 1 && richMediaDownloadMap[downloadId]) {
+        richMediaDownloadMap[downloadId]!(notifyInfo.filePath)
+        delete richMediaDownloadMap[downloadId]
+      }
+
+      return
+    }
+
+    case 'nodeIKernelProfileListener/onProfileSimpleChanged':
+    case 'nodeIKernelProfileListener/onProfileDetailInfoChanged':
+    case 'nodeIKernelGroupListener/onSearchMemberChange':
+    case 'nodeIKernelGroupService/getNextMemberList': {
+      // const authData = await ctx.chronocat.getAuthData()
+
+      const { profiles, infos } = payload as OnProfileChanged
+
+      // if (profiles.get(authData.uid))
+      //   selfProfile.value = profiles.get(authData.uid)
+
+      const profile = profiles ?? infos
+      for (const [uid, { uin }] of profile) ctx.chronocat.uix.add(uid, uin)
+
+      return
+    }
+
+    case 'nodeIKernelGroupListener/onMemberInfoChange': {
+      const { members } = payload as OnMemberInfoChange
+
+      for (const [uid, { uin }] of members) {
+        ctx.chronocat.uix.add(uid, uin)
+      }
+      return
+    }
+
+    case 'nodeIKernelGroupListener/onMemberListChange': {
+      const { info } = payload as OnMemberListChange
+
+      const groupCode = info.sceneId.split('_')[0]
+      if (!groupCode) return
+
+      for (const [uid, { uin }] of info.infos) {
+        ctx.chronocat.uix.add(uid, uin)
+      }
+
+      return
+    }
+
+    case 'onGroupListUpdate':
+    case 'nodeIKernelGroupListener/onGroupListUpdate': {
+      const { groupList } = payload as OnGroupListUpdate
+
+      for (const group of groupList) groupMap[group.groupCode] = group
+
+      chronoEventEmitter.emitGroupListUpdate()
+
+      return
+    }
+
+    case 'onBuddyListChange':
+    case 'nodeIKernelBuddyListener/onBuddyListChange': {
+      const { data } = payload as OnBuddyListChange
+
+      for (const category of data) {
+        for (const buddy of category.buddyList) {
+          ctx.chronocat.uix.add(buddy.uid, buddy.uin)
+
+          // buddy.category = category.categoryName
+          friendMap[buddy.uin] = buddy
+        }
+      }
+
+      return
+    }
+
+    case 'nodeIKernelMsgService/getMsgsIncludeSelf': {
+      const { msgList } = payload as MsgsIncludeSelf
+
+      for (const msg of msgList) {
+        ctx.chronocat.uix.add(msg.senderUid, msg.senderUin)
+        if (msg.chatType === ChatType.Private)
+          ctx.chronocat.uix.add(msg.peerUid, msg.peerUin)
+      }
+
+      return
+    }
+
+    case 'nodeIKernelRecentContactListener/onRecentContactListChangedVer2': {
+      const { changedRecentContactLists } =
+        payload as OnRecentContactListChangedVer2
+
+      for (const changedRecentContactList of changedRecentContactLists)
+        for (const contact of changedRecentContactList.changedList) {
+          ctx.chronocat.uix.add(contact.senderUid, contact.senderUin)
+          if (contact.chatType === ChatType.Private)
+            ctx.chronocat.uix.add(contact.peerUid, contact.peerUin)
+        }
+
+      return
+    }
+
+    case 'onOpenParamChange': {
+      const { data } = payload as OnOpenParamChange
+
+      for (const contact of data) {
+        ctx.chronocat.uix.add(contact.senderUid, contact.senderUin)
+        if (contact.chatType === ChatType.Private)
+          ctx.chronocat.uix.add(contact.peerUid, contact.peerUin)
+      }
+
+      return
+    }
+
+    case 'nodeIKernelMsgListener/onAddSendMsg': {
+      const { msgRecord } = payload as OnAddSendMsg
+      // msgRecord.sendStatus === 1, sending
+      sendCallbackMap[msgRecord.msgId] = sendQueue.shift()!
+      return
+    }
+
+    case 'nodeIKernelMsgListener/onMsgInfoListUpdate': {
+      const { msgList } = payload as OnMsgInfoListUpdate
+      for (const msg of msgList) {
+        if (msg.sendStatus > 1) {
+          sendCallbackMap[msg.msgId]?.(msg)
+          delete sendCallbackMap[msg.msgId]
+        }
+      }
+
+      return
+    }
+  }
+}
---
+++ packages/engine-chronocat-event/src/handler.ts
@@ -0,0 +1,268 @@
+import type {
+  Element,
+  MsgsIncludeSelf,
+  OnBuddyListChange,
+  OnBuddyReqChange,
+  OnMemberInfoChange,
+  OnMemberListChange,
+  OnMsgInfoListUpdate,
+  OnOpenParamChange,
+  OnProfileChanged,
+  OnRecentContactListChangedVer2,
+  OnRecvMsg,
+  Peer,
+  RedIpcDataEvent,
+  RedIpcDataRequest,
+  RedMessage,
+} from '@chronocat/red'
+import { ChatType, MsgType, SendType } from '@chronocat/red'
+import type { ChronocatContext } from '@chronocat/shell'
+import type { IpcManData } from 'ipcman'
+import { emittedBuddyReqList, requestMethodMap } from './globalVars'
+import {
+  FriendRequestDispatchMessage,
+  MessageCreatedDispatchMessage,
+  MessageDeletedDispatchMessage,
+} from './messages'
+
+export const buildHandler = (ctx: ChronocatContext) => (data: IpcManData) => {
+  switch (data.type) {
+    case 'event': {
+      if (!data.args[1] || !Array.isArray(data.args[1])) return
+      const d = data.args[1] as [RedIpcDataEvent]
+      if (!d.length) return
+      const e = d[0]
+      if (!e || !('cmdName' in e)) return
+      void dispatcher(ctx, e.cmdName, e.payload)
+      return
+    }
+
+    case 'wrapped-request': {
+      requestMethodMap[data.id] = (data.args[1] as RedIpcDataRequest)[0]
+      return
+    }
+
+    case 'wrapped-response': {
+      const method = requestMethodMap[data.id]
+      if (!method) return
+      delete requestMethodMap[data.id]
+      void dispatcher(ctx, method, data.args[1] /* RedIpcDataResponse */)
+      return
+    }
+  }
+}
+
+const dispatcher = async (
+  ctx: ChronocatContext,
+  method: string,
+  payload: unknown,
+) => {
+  switch (method) {
+    case 'nodeIKernelMsgListener/onRecvMsg': {
+      const { msgList } = payload as OnRecvMsg
+
+      for (const msg of msgList) {
+        ctx.chronocat.uix.add(msg.senderUid, msg.senderUin)
+        if (msg.chatType === ChatType.Private)
+          ctx.chronocat.uix.add(msg.peerUid, msg.peerUin)
+      }
+
+      // const prepareRole = async (msg: Message) => {
+      //   if (msg.chatType === ChatType.Group) {
+      //     await getMemberInfo({
+      //       forceUpdate: false,
+      //       groupCode: +msg.peerUid,
+      //       uids: [msg.senderUid],
+      //     })
+      //   }
+      // }
+
+      // const fillRole = (msg: Message) => {
+      //   if (msg.chatType === ChatType.Group) {
+      //     msg.roleType = roleMap[msg.peerUid]?.[msg.senderUin]
+      //   }
+      // }
+
+      const filteredPayload = await Promise.all(
+        msgList.filter(filterMessage).map(async (msg) => {
+          // await prepareRole(msg)
+          // msg = await uixCache.preprocessObject(msg)
+          // setMsgCache(msg)
+          // fillRole(msg)
+          return msg
+        }),
+      )
+
+      if (filteredPayload.length)
+        ctx.chronocat.emit(new MessageCreatedDispatchMessage(filteredPayload))
+
+      return
+    }
+
+    case 'nodeIKernelProfileListener/onProfileSimpleChanged':
+    case 'nodeIKernelProfileListener/onProfileDetailInfoChanged':
+    case 'nodeIKernelGroupListener/onSearchMemberChange':
+    case 'nodeIKernelGroupService/getNextMemberList': {
+      // const authData = await ctx.chronocat.getAuthData()
+
+      const { profiles, infos } = payload as OnProfileChanged
+
+      // if (profiles.get(authData.uid))
+      //   selfProfile.value = profiles.get(authData.uid)
+
+      const profile = profiles ?? infos
+      for (const [uid, { uin }] of profile) ctx.chronocat.uix.add(uid, uin)
+
+      return
+    }
+
+    case 'nodeIKernelGroupListener/onMemberInfoChange': {
+      const { members } = payload as OnMemberInfoChange
+
+      for (const [uid, { uin }] of members) {
+        ctx.chronocat.uix.add(uid, uin)
+
+        // if (!(groupCode in roleMap)) roleMap[groupCode] = {}
+        // roleMap[groupCode][uin] = role
+      }
+      return
+    }
+
+    case 'nodeIKernelGroupListener/onMemberListChange': {
+      const { info } = payload as OnMemberListChange
+
+      const groupCode = info.sceneId.split('_')[0]
+      if (!groupCode) return
+
+      for (const [uid, { uin }] of info.infos) {
+        ctx.chronocat.uix.add(uid, uin)
+
+        // if (!(groupCode in roleMap)) roleMap[groupCode] = {}
+        // roleMap[groupCode][uin] = role
+      }
+
+      return
+    }
+
+    case 'nodeIKernelRecentContactListener/onRecentContactListChangedVer2': {
+      const { changedRecentContactLists } =
+        payload as OnRecentContactListChangedVer2
+
+      for (const changedRecentContactList of changedRecentContactLists)
+        for (const contact of changedRecentContactList.changedList) {
+          ctx.chronocat.uix.add(contact.senderUid, contact.senderUin)
+          if (contact.chatType === ChatType.Private)
+            ctx.chronocat.uix.add(contact.peerUid, contact.peerUin)
+        }
+
+      return
+    }
+
+    case 'onOpenParamChange': {
+      const { data } = payload as OnOpenParamChange
+
+      for (const contact of data) {
+        ctx.chronocat.uix.add(contact.senderUid, contact.senderUin)
+        if (contact.chatType === ChatType.Private)
+          ctx.chronocat.uix.add(contact.peerUid, contact.peerUin)
+      }
+
+      return
+    }
+
+    case 'nodeIKernelMsgService/getMsgsIncludeSelf': {
+      const { msgList } = payload as MsgsIncludeSelf
+
+      for (const msg of msgList) {
+        ctx.chronocat.uix.add(msg.senderUid, msg.senderUin)
+        if (msg.chatType === ChatType.Private)
+          ctx.chronocat.uix.add(msg.peerUid, msg.peerUin)
+      }
+
+      return
+    }
+
+    case 'onBuddyListChange':
+    case 'nodeIKernelBuddyListener/onBuddyListChange': {
+      const { data } = payload as OnBuddyListChange
+
+      for (const category of data) {
+        for (const buddy of category.buddyList) {
+          ctx.chronocat.uix.add(buddy.uid, buddy.uin)
+        }
+      }
+
+      return
+    }
+
+    case 'nodeIKernelBuddyListener/onBuddyReqChange': {
+      const { buddyReqs } = payload as OnBuddyReqChange
+
+      buddyReqs.forEach((x) => {
+        if (x.reqType !== 1 || x.reqSubType !== 1) return
+
+        const uin = ctx.chronocat.uix.getUin(x.friendUid)
+        if (!uin) return
+
+        const key = `${uin}:${x.reqTime}`
+        if (emittedBuddyReqList.includes(key)) return
+
+        emittedBuddyReqList.push(key)
+
+        ctx.chronocat.emit(new FriendRequestDispatchMessage(x, uin))
+      })
+
+      return
+    }
+
+    case 'nodeIKernelMsgListener/onMsgInfoListUpdate': {
+      const { msgList } = payload as OnMsgInfoListUpdate
+
+      const filteredPayload = await Promise.all(
+        msgList
+          .filter(
+            (x) =>
+              x.msgType === MsgType.System &&
+              x.subMsgType === 4 &&
+              !x.isOnlineMsg &&
+              Number(x.recallTime) &&
+              x.elements[0]!.elementType === 8 &&
+              x.elements[0]!.grayTipElement?.subElementType === 1,
+          )
+          .filter(filterMessage)
+          .map(async (msg) => {
+            // await prepareRole(msg)
+            // msg = await uixCache.preprocessObject(msg)
+            // setMsgCache(msg)
+            // fillRole(msg)
+            return msg
+          }),
+      )
+      if (filteredPayload.length)
+        ctx.chronocat.emit(new MessageDeletedDispatchMessage(filteredPayload))
+      return
+    }
+  }
+}
+
+export const filterMessage = (
+  message:
+    | {
+        peer: Peer
+        elements: Element[]
+      }
+    | RedMessage,
+) =>
+  'peer' in message
+    ? !message.elements.some((x) => x.walletElement || x.arkElement)
+    : !(
+        message.msgType === MsgType.Ark ||
+        message.msgType === MsgType.Wallet ||
+        (message.msgType === MsgType.System &&
+          message.subMsgType === 17 &&
+          message.sendType === SendType.System &&
+          message.elements[0]!.elementType === 8 &&
+          message.elements[0]!.grayTipElement!.subElementType === 16 &&
+          message.elements[0]!.grayTipElement!.jsonGrayTipElement!.busiId ===
+            '81')
+      )
