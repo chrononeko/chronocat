b11abb564a133bf4d213cbebd6a1ec1c2331466a
Il Harper <hi@ilharper.com>
2024-03-09 20:41:26 +0800

    refactor: use `esbuild-register`

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -4,11 +4,6 @@
 
 'use strict'
 
-require('ts-node').register({
-  scope: true,
-  scopeDir: __dirname,
-  swc: true,
-  transpileOnly: true,
-})
+require('esbuild-register/dist/node').register()
 
 module.exports = require('./config').config
