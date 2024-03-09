3f6414d624100b0db52f1b37aed963baa45af921
impart我的impart呢 <noimpart@no.life>
2024-03-09 11:02:33 +0800

    chore: add prettier .vscode config

--- packages/shell/src/services/api.ts
+++ packages/shell/src/services/api.ts
@@ -29,16 +29,21 @@ export type Api = {
 
 const buildNotimpl = (name: string) => {
   const fn = () =>
-    l.error(new Error(`没有引擎提供 ${bgMagenta(white(name))}。可能没有加载所需的引擎？`), {
-      code: 2159,
-      throw: true,
-    })
+    l.error(
+      new Error(
+        `没有引擎提供 ${bgMagenta(white(name))}。可能没有加载所需的引擎？`,
+      ),
+      {
+        code: 2159,
+        throw: true,
+      },
+    )
 
-    ; (
-      fn as unknown as {
-        [notimplSym]: boolean
-      }
-    )[notimplSym] = true
+  ;(
+    fn as unknown as {
+      [notimplSym]: boolean
+    }
+  )[notimplSym] = true
 
   return fn
 }
@@ -46,7 +51,7 @@ const buildNotimpl = (name: string) => {
 const handler: ProxyHandler<Api> = {
   get: (target, name) =>
     typeof name === 'symbol' ||
-      Object.prototype.hasOwnProperty.call(target, name)
+    Object.prototype.hasOwnProperty.call(target, name)
       ? target[name as keyof Methods]
       : buildNotimpl(name),
 }
@@ -55,20 +60,23 @@ export const api = new Proxy({} as Api, handler)
 
 api.register =
   (engine: string, defaultPriority: number = 0) =>
-    <M extends keyof Methods>(
-      method: M,
-      impl: (...args: Methods[M][0]) => Promise<Methods[M][1]>,
-      priority: number = -1,
-    ) => {
-      const newPriority = priority === -1 ? defaultPriority : priority
-      if (api[method]) {
-        l.warn(`${cyan(engine)}(${newPriority}) 与 ${cyan(api[method].engine)}(${api[method].priority
-          }) 重复注册了方法 ${bgMagenta(white(method))}，将采用 ${newPriority > api[method].priority ? engine : api[method].engine} 的版本。`)
+  <M extends keyof Methods>(
+    method: M,
+    impl: (...args: Methods[M][0]) => Promise<Methods[M][1]>,
+    priority: number = -1,
+  ) => {
+    const newPriority = priority === -1 ? defaultPriority : priority
+    if (api[method]) {
+      l.warn(
+        `${cyan(engine)}(${newPriority}) 与 ${cyan(api[method].engine)}(${
+          api[method].priority
+        }) 重复注册了方法 ${bgMagenta(white(method))}，将采用 ${newPriority > api[method].priority ? engine : api[method].engine} 的版本。`,
+      )
 
-        if (newPriority < api[method].priority) return
-      }
-      // FIXME: Do not use type assertion
-      api[method] = impl /* ApiImpl<M> */ as Api[M]
-      api[method].engine = engine
-      api[method].priority = newPriority
+      if (newPriority < api[method].priority) return
     }
+    // FIXME: Do not use type assertion
+    api[method] = impl /* ApiImpl<M> */ as Api[M]
+    api[method].engine = engine
+    api[method].priority = newPriority
+  }
