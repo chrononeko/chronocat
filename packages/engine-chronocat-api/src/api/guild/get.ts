1311e1b5d131a6ecd0493fe62ba274ac7b0ce9bc
Il Harper <hi@ilharper.com>
2024-03-10 14:20:41 +0800

    feat(api): guild.get get name

--- packages/engine-chronocat-api/src/api/guild/get.ts
+++ packages/engine-chronocat-api/src/api/guild/get.ts
@@ -1,11 +1,20 @@
 import type { ChronocatContext, Guild, GuildGetPayload } from '@chronocat/shell'
+import { groupMap } from '../../globalVars'
 
 export const buildGuildGet =
   (_ctx: ChronocatContext) =>
   async ({ guild_id }: GuildGetPayload): Promise<Guild> => {
-    return {
+    const result: Guild = {
       id: guild_id,
       name: undefined as unknown as string,
       avatar: `https://p.qlogo.cn/gh/${guild_id}/${guild_id}/640`,
     }
+
+    const group = groupMap[guild_id]
+
+    if (group) {
+      result.name = group.groupName
+    }
+
+    return result
   }
