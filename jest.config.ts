a43066f17b66a54d7f1761374254a7f4caea6d9e
Il Harper <hi@ilharper.com>
2024-03-13 22:21:27 +0800

    test: fix esm packages

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { JestConfigWithTsJest } from 'ts-jest'
 const config: JestConfigWithTsJest = {
   preset: 'ts-jest/presets/default-esm',
   moduleNameMapper: {
+    '@chronocat/red': '@chronocat/red/src',
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   transform: {
