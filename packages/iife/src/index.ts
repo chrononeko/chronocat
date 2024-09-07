519145b5a849f2dce6311c4478439feed453772b
Il Harper <hi@ilharper.com>
2024-09-07 15:33:57 +0800

    feat(iife): include engine media

--- packages/iife/src/index.ts
+++ packages/iife/src/index.ts
@@ -1,5 +1,6 @@
 import * as ccApi from '@chronocat/engine-chronocat-api'
 import * as ccEvent from '@chronocat/engine-chronocat-event'
+import * as media from '@chronocat/engine-media'
 import { chronocat } from '@chronocat/shell'
 
 const octx = chronocat()
@@ -7,4 +8,5 @@ const octx = chronocat()
 if (octx) {
   octx.load(ccApi)
   octx.load(ccEvent)
+  octx.load(media)
 }
