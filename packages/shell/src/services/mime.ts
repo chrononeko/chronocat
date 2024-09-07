a91e4bde1977c74db790541f5c71e861e7841b70
Il Harper <hi@ilharper.com>
2024-09-07 14:15:22 +0800

    fix(shell): expect hoist pollution

--- packages/shell/src/services/mime.ts
+++ packages/shell/src/services/mime.ts
@@ -1,5 +1,7 @@
-// Should be 'mime/lite' but there's hoist pollution
-import { Mime } from 'mime/dist/src/index_lite'
+// @ts-expect-error
+// There's a hoist pollution (@types/mime). see
+// https://github.com/Microsoft/TypeScript/issues/11917
+import { Mime } from 'mime/lite'
 import mimeStandard from 'mime/types/standard.js'
 
 export const mime = new Mime().define(mimeStandard).define({
